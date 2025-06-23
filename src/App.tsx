c9d93d677319bb42a5557f5297152c42bfc96726
PremChand34555 <premthakurfact786@gmail.com>
2025-06-23 06:46:20 +0500

    Add link

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import BlogGenerator from './pages/BlogGenerator';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<BlogGenerator />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<BlogGenerator />} />
+    </Routes>
   );
 }
 
